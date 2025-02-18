4bf3821050820fca531997a64a9f1a4fee51b722
ChengZ3 <chengz3@andrew.cmu.edu>
2025-02-17 20:59:35 -0500

    organize test files

---
+++ tests/by_lean.rs
@@ -0,0 +1,29 @@
+use vstd::prelude::*;
+
+verus! {
+
+proof fn lean_test(x: int, y: int, z:int, m: int) by (lean)
+  requires ((x-y) % m == 0)
+  ensures (x*z- y*z) % m == 0
+{}
+
+proof fn assert_lean_test(x:int, y:int) {
+  assert(x == y ==> y == x) by (lean);
+  assert(x + y == y + x) by (lean);
+}
+
+proof fn assert_lean_jumble(x:int, y:int, z:bool, a:u32) {
+  assert(x + y < y + x + x) by (lean);
+  assert(x * y < y / x) by (lean);
+  assert(!a & (a ^ a) == (a << 1) | (a >> 1)) by (lean);
+}
+
+// proof fn test_this(i: int) {
+//   assert(let j := i + 1 in j + j != 2 * i + 1) by (lean);
+// }
+
+fn main() {
+  assert(true);
+}
+
+}
